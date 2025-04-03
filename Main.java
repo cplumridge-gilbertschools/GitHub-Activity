d35886f47b124fb7372d21c9607cba50b4644bbb
cplumridge-gilbertschools <cplumridge@gilbertschools.net>
2025-04-03 08:38:50 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
     ArrayList list = new ArrayList();
     for (i=2; i<10; i++) {
-      list.add("Hello World!");
+      list.add("Hello!");
     }
     printAll(list);
   }
