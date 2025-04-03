ed628e3ef308d66b9c7aed0e3ac2d46a1d0fc0a6
cplumridge-gilbertschools <cplumridge@gilbertschools.net>
2025-04-03 08:26:34 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    ArrayList list = new ArrayList();
+    for (i=0; i<10; i++) {
+      list.add("Hello World!");
+    }
+    printAll(list);
+  }
+
+  public static void printAll(ArrayList list) {
+    for (Object obj : list) {
+      System.out.println(obj);
+    }
+  }
+}
